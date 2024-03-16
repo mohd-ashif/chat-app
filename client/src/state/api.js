03f1321006e06ee4f9732f1bca5ad127a85141c0
mohd-ashif <mohdaszif077@gmail.com>
2024-03-16 12:43:34 +0530

    set-up store

---
+++ client/src/state/api.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: [],
+  endpoints: (builder) => ({
+    postAiText: builder.mutation({
+      query: (payload) => ({
+        url: "openai/text",
+        method: "POST",
+        body: payload,
+      }),
+    }),
+  }),
+});
+
+export const { usePostAiTextMutation } = api;
